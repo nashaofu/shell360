057169c21d22b598a2ead0728ee2f5e029a97167
nashaofu <19303058+nashaofu@users.noreply.github.com>
2025-09-13 02:49:39 +0800

    更新 ci

--- scripts/linux.sh
+++ scripts/linux.sh
@@ -3,19 +3,9 @@
 set -e
 
 rustup target add x86_64-unknown-linux-gnu aarch64-unknown-linux-gnu
-sudo apt-get update
-sudo apt-get install -y \
-  build-essential \
-  gcc-aarch64-linux-gnu \
-  libwebkit2gtk-4.1-dev \
-  curl \
-  wget \
-  file \
-  libssl-dev \
-  librsvg2-dev \
-  libgtk-3-dev \
-  libwebkit2gtk-4.1-dev \
-  libayatana-appindicator3-dev
+
+./install_linux_deps.sh
+
 pnpm tauri build --target x86_64-unknown-linux-gnu
 # pnpm tauri build --target aarch64-unknown-linux-gnu
 rm -rf build
