e9b79c4b4a99684b1aed7a2b2eb5a077f0a33072
nashaofu <19303058+nashaofu@users.noreply.github.com>
2025-09-25 09:26:58 +0800

    feat: 重新生成安卓项目

--- src-tauri/gen/android/app/src/main/java/com/nashaofu/shell360/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/nashaofu/shell360/MainActivity.kt
@@ -1,3 +1,11 @@
 package com.nashaofu.shell360
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
