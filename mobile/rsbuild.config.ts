7804fbdc1a9d0437a52043b3ecccd20f62767474
nashaofu <19303058+nashaofu@users.noreply.github.com>
2026-06-07 22:28:32 +0800

    chore: 添加 @rsbuild/plugin-svgr 插件并更新配置

--- mobile/rsbuild.config.ts
+++ mobile/rsbuild.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginLess } from "@rsbuild/plugin-less";
 import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginLess()],
+  plugins: [
+    pluginReact(),
+    pluginLess(),
+    pluginSvgr({
+      svgrOptions: {
+        exportType: "named",
+      },
+      mixedImport: true,
+    }),
+  ],
   source: {
     define: {
       "import.meta.env.TAURI_ENV_PLATFORM": JSON.stringify(
